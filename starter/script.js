8b240f3e236a67d2ac664c795ba52c4c2d98a22c
Christian <seorefchristian@gmail.com>
2025-03-26 21:43:57 +0300

    Refactore it

--- starter/script.js
+++ starter/script.js
@@ -6,12 +6,19 @@ const score1El = document.querySelector('#score--1');
 const current0El = document.querySelector('#current--0');
 const current1El = document.querySelector('#current--1');
 
+const player1 = document.querySelector('.player--0');
+const player2 = document.querySelector('.player--1');
+const textPlayer1 = document.querySelector('#name--0');
+const textPlayer2 = document.querySelector('#name--1');
+const player = document.querySelectorAll('.player');
+
 const dice = document.querySelector('.dice');
 dice.classList.add('hidden'); //hide the dice for now
 
 //Secting btn to roll dice and hiold score
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+const btnNew = document.querySelector('.btn--new');
 
 //Assigning different value
 let totalScore0 = 0;
@@ -27,14 +34,12 @@ function diceValue() {
 
 //Display diceroll
 function displayDice(diceValue) {
-  const dice = document.querySelector('.dice');
   dice.classList.remove('hidden');
-  dice.setAttribute('src', `dice-${diceValue}.png`);
+  dice.src = `dice-${diceValue}.png`;
 }
 
 //checking what player--active
 function checkActivePlayer() {
-  const player = document.querySelectorAll('.player');
   for (let index = 0; index < player.length; index++) {
     const element = player[index];
     if (element.classList.contains('player--active')) {
@@ -69,15 +74,15 @@ function displayScore(currentScore) {
 }
 
 //Add current score to total score
-function addCurrentScoreToTotal(){
+function addCurrentScoreToTotal() {
   const activePlayer = checkActivePlayer();
-  if(activePlayer === 1){
+  if (activePlayer === 1) {
     totalScore0 += currentScore;
     score0El.textContent = totalScore0;
     currentScore = 0;
     current0El.textContent = currentScore;
     switchPlayer();
-  } else{
+  } else {
     totalScore1 += currentScore;
     score1El.textContent = totalScore1;
     currentScore = 0;
@@ -87,29 +92,25 @@ function addCurrentScoreToTotal(){
 }
 
 //Define who is the winner
-function defineWinner(){
-  if (totalScore0 >= 100){
-    const player1 = document.querySelector('.player--0')
-    player1.style.backgroundColor = '#2f2f2f';
-
-  }else if(totalScore1 >= 100) {
-    const player2 = document.querySelector('.player--1')
-    player2.style.backgroundColor = '#2f2f2f';
+function defineWinner() {
+  if (totalScore0 >= 100) {
+    textPlayer1.style.color = '#c7365f';
+    textPlayer1.style.fontWeight = '700';
+    player1.classList.add('player--winner');
+  } else if (totalScore1 >= 100) {
+    textPlayer2.style.fontWeight = '700';
+    textPlayer2.style.color = '#c7365f';
+    player2.classList.add('player--winner');
     // console.log(totalScore1);
   }
-
 }
 
 //ROLLING THE DICE
 btnRoll.addEventListener('click', function () {
-  
   //See who is playing (Player 1 or 2)
   const activePlayer = checkActivePlayer();
-
   //Generating random dice roll
   const randomValue = diceValue();
-
-
   //display dice roll (Image)
   displayDice(randomValue);
 
@@ -122,7 +123,6 @@ btnRoll.addEventListener('click', function () {
     activePlayer === 1
       ? (current0El.textContent = currentScore)
       : (current1El.textContent = currentScore);
-
   }
   //Switch to another player
   else {
@@ -135,17 +135,30 @@ btnRoll.addEventListener('click', function () {
       ? (current0El.textContent = currentScore)
       : (current1El.textContent = currentScore);
   }
-
 });
 
 //HOLDING SCORE
 btnHold.addEventListener('click', function () {
-
   //Add current score to total score
   addCurrentScoreToTotal();
-defineWinner();
-
-
+  defineWinner();
 });
 
+//START NEW GAME
+btnNew.addEventListener('click', function () {
+  totalScore0 = 0;
+  totalScore1 = 0;
+  score0El.textContent = totalScore0;
+  current0El.textContent = totalScore0;
+  score1El.textContent = totalScore1;
+  current1El.textContent = totalScore1;
+  currentScore = 0;
+  dice.classList.add('hidden');
 
+  for (let index = 0; index < player.length; index++) {
+    const element = player[index];
+    if (element.classList.contains('player--winner')) {
+      element.classList.remove('player--winner');
+    }
+  }
+});
